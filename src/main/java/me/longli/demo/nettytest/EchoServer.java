1335865ee250b8a8ac917d09843353b2e92dde05
Longli (Long Juncheng) <longcpp@gmail.com>
2023-04-03 21:39:46 +0800

    netty EchoServer

---
+++ src/main/java/me/longli/demo/nettytest/EchoServer.java
@@ -0,0 +1,52 @@
+package me.longli.demo.nettytest;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.Future;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        final EchoServerHandler myHandler = new EchoServerHandler();
+        final EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ServerBootstrap b1 = b.group(group);
+            ServerBootstrap b2 = b.channel(NioServerSocketChannel.class); // 指定使用NIO传输channel
+            ServerBootstrap b3 = b.localAddress(port); // 指定端口
+            ServerBootstrap b4 = b.childHandler(new ChannelInitializer<>() { // 将channelHandler添加到channelPipeline // TODO 与 b.childHandler(new EchoServerHandler()); 的区别在哪？
+                @Override
+                protected void initChannel(Channel ch) throws Exception {
+                    ch.pipeline().addLast(myHandler);
+                }
+            });
+            ChannelFuture bindFuture = b.bind(); // 异步地绑定服务器
+            ChannelFuture syncBindFuture = bindFuture.sync(); // 同步等待
+
+            Channel channel = syncBindFuture.channel();
+            ChannelFuture closeFuture = channel.closeFuture();
+            ChannelFuture syncCloseFuture = closeFuture.sync(); // TODO 什么时候会执行close呢？
+
+            System.out.println("EchoServer end. b=" + (b==b1 && b1==b2 && b2==b3 && b3==b4));
+        } finally {
+            Future<?> shutdownFuture = group.shutdownGracefully();
+            shutdownFuture.sync();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = Integer.parseInt(args[0]);
+        EchoServer server = new EchoServer(port);
+        server.start();
+    }
+}
--- src/main/java/me/longli/demo/nettytest/EchoServerHandler.java
+++ src/main/java/me/longli/demo/nettytest/EchoServerHandler.java
@@ -2,6 +2,7 @@ package me.longli.demo.nettytest;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -17,15 +18,15 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf) msg;
         System.out.println("服务端接收到消息：" + in.toString(CharsetUtil.UTF_8));
-        ctx.write(in);
+        ctx.write(in); // 将消息发送回对方
 
         WebDemoApplication.publisher.publishEvent(new MyEvent(this, "channelRead", LocalDateTime.now()));
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
-                //.addListener(ChannelFutureListener.CLOSE);
+        ChannelFuture channelFuture = ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);// 将未决消息冲刷到远程节点
+        channelFuture.addListener(ChannelFutureListener.CLOSE); // 并且关闭该Channel
 
         WebDemoApplication.publisher.publishEvent(new MyEvent(this, "channelReadComplete", LocalDateTime.now()));
     }
@@ -33,7 +34,7 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
-        ctx.close();
+        ctx.close(); // 关闭该channel
 
         WebDemoApplication.publisher.publishEvent(new MyEvent(this, "exceptionCaught", LocalDateTime.now()));
     }
