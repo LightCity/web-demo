999c7a4192ad08663e3b18866c3f643db5b1cc3e
Longli (Long Juncheng) <longcpp@gmail.com>
2023-04-10 20:43:17 +0800

    xxoo

--- src/main/java/me/longli/demo/generic/JavaSuperExtendKeyword.java
+++ src/main/java/me/longli/demo/generic/JavaSuperExtendKeyword.java
@@ -1,5 +1,10 @@
 package me.longli.demo.generic;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.List;
 
 @SuppressWarnings("all")
@@ -14,4 +19,95 @@ public class JavaSuperExtendKeyword {
     static List<? extends Fruit> aFruitList; // aFruitList 只能get；不能add
 
     static List<? super   Apple> bFruitList; // bFruitList 只能add，不能get
+
+    //==================================================
+    static class A<T1, T2> {}
+
+    static class B<T2> extends A<String, T2> {}
+
+    static class C extends B<Long>{}
+
+    public static void main(String[] args) {
+        C c = new C();
+        Class<?> t1 = find0(c, A.class, "T1");
+        Class<?> t2 = find0(c, B.class, "T2");
+//        Class<?> t3 = find0(c, B.class, "T1");
+        System.out.println("t1=" + t1.getName());
+        System.out.println("t2=" + t2.getName());
+//        System.out.println("t3=" + t3.getName());
+    }
+
+    private static Class<?> find0(
+            final Object object, Class<?> parametrizedSuperclass, String typeParamName) {
+
+        final Class<?> thisClass = object.getClass();
+        Class<?> currentClass = thisClass;
+        for (;;) {
+            if (currentClass.getSuperclass() == parametrizedSuperclass) {
+                int typeParamIndex = -1;
+                TypeVariable<?>[] typeParams = currentClass.getSuperclass().getTypeParameters();
+                for (int i = 0; i < typeParams.length; i ++) {
+                    if (typeParamName.equals(typeParams[i].getName())) {
+                        typeParamIndex = i;
+                        break;
+                    }
+                }
+
+                if (typeParamIndex < 0) {
+                    throw new IllegalStateException(
+                            "unknown type parameter '" + typeParamName + "': " + parametrizedSuperclass);
+                }
+
+                Type genericSuperType = currentClass.getGenericSuperclass();
+                if (!(genericSuperType instanceof ParameterizedType)) {
+                    return Object.class;
+                }
+
+                Type[] actualTypeParams = ((ParameterizedType) genericSuperType).getActualTypeArguments();
+
+                Type actualTypeParam = actualTypeParams[typeParamIndex];
+                if (actualTypeParam instanceof ParameterizedType) {
+                    actualTypeParam = ((ParameterizedType) actualTypeParam).getRawType();
+                }
+                if (actualTypeParam instanceof Class) {
+                    return (Class<?>) actualTypeParam;
+                }
+                if (actualTypeParam instanceof GenericArrayType) {
+                    Type componentType = ((GenericArrayType) actualTypeParam).getGenericComponentType();
+                    if (componentType instanceof ParameterizedType) {
+                        componentType = ((ParameterizedType) componentType).getRawType();
+                    }
+                    if (componentType instanceof Class) {
+                        return Array.newInstance((Class<?>) componentType, 0).getClass();
+                    }
+                }
+                if (actualTypeParam instanceof TypeVariable) {
+                    // Resolved type parameter points to another type parameter.
+                    TypeVariable<?> v = (TypeVariable<?>) actualTypeParam;
+                    if (!(v.getGenericDeclaration() instanceof Class)) {
+                        return Object.class;
+                    }
+
+                    currentClass = thisClass;
+                    parametrizedSuperclass = (Class<?>) v.getGenericDeclaration();
+                    typeParamName = v.getName();
+                    if (parametrizedSuperclass.isAssignableFrom(thisClass)) {
+                        continue;
+                    }
+                    return Object.class;
+                }
+
+                return fail(thisClass, typeParamName);
+            }
+            currentClass = currentClass.getSuperclass();
+            if (currentClass == null) {
+                return fail(thisClass, typeParamName);
+            }
+        }
+    }
+
+    private static Class<?> fail(Class<?> type, String typeParamName) {
+        throw new IllegalStateException(
+                "cannot determine the type of the type parameter '" + typeParamName + "': " + type);
+    }
 }
---
+++ src/main/java/me/longli/demo/nettytest/EchoServerOutboundHandler.java
@@ -0,0 +1,6 @@
+package me.longli.demo.nettytest;
+
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+
+public class EchoServerOutboundHandler extends ChannelOutboundHandlerAdapter {
+}
